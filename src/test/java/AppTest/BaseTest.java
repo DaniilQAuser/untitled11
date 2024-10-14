cd15eb2a2e4f464ebfe2548bc4f9aec99082b41b
DANIIL1 <daniilgamilov1@gmail.com>
2024-10-14 14:03:16 +0500

    Поменял юрл 111

--- src/test/java/AppTest/BaseTest.java
+++ src/test/java/AppTest/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("app", "C:/BuildAndroid/ru.vkusvill-3.9.1-lollipop-release (1).apk");
 
-        URL appiumServerURL = new URL("http://127.0.0.1:4723/wd/hub");
+        URL appiumServerURL = new URL("http://127.0.0.1:4444/wd/hub");
 
         driver = new AndroidDriver(appiumServerURL, capabilities);
     }
@@ -40,7 +40,7 @@ public class BaseTest {
             driver.quit();
         }
     }
-    // Метод для ожидания видимости элемента
+    // Метод для ожидания видимости элемента локатора
     public WebElement waitForVisibility(By locator) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
