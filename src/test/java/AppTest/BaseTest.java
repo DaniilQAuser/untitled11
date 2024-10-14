289dbdb8ce1dbfe1cc1cbc00350dfd48a8983dc3
DaniilQAuser <154884972+DaniilQAuser@users.noreply.github.com>
2024-10-14 14:02:13 +0500

    Update BaseTest.java

--- src/test/java/AppTest/BaseTest.java
+++ src/test/java/AppTest/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("app", "C:/BuildAndroid/ru.vkusvill-3.9.1-lollipop-release (1).apk");
 
-        URL appiumServerURL = new URL("http://127.0.0.1:8888/wd/hub");
+        URL appiumServerURL = new URL("http://127.0.0.1:1444/wd/hub");
 
         driver = new AndroidDriver(appiumServerURL, capabilities);
     }
@@ -40,7 +40,7 @@ public class BaseTest {
             driver.quit();
         }
     }
-    // Метод для ожидания видимости элемента
+    // Метод 
     public WebElement waitForVisibility(By locator) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
