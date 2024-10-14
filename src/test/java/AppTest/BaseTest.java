727d526f5fe86ee3c43948d6810b550029886f45
DANIIL1 <daniilgamilov1@gmail.com>
2024-10-14 06:13:20 +0500

    Обновил капабилити

--- src/test/java/AppTest/BaseTest.java
+++ src/test/java/AppTest/BaseTest.java
@@ -28,9 +28,9 @@ public class BaseTest {
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "AndroidSDK");
         capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("app", "C:/Games/vkusvill.apk");
+        capabilities.setCapability("app", "C:/BuildAndroid/ru.vkusvill-3.9.1-lollipop-release (1).apk");
 
-        URL appiumServerURL = new URL("http://192.168.0.105:4723/wd/hub");
+        URL appiumServerURL = new URL("http://127.0.0.1:4723/wd/hub");
 
         driver = new AndroidDriver(appiumServerURL, capabilities);
     }
